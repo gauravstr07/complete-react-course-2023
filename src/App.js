bf6f19e516bdac8718445450cfa33dbe465f181b
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2023-01-10 20:05:59 +0530

    React Movie Website Using Props | React.js Full Course #8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
-
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import movies from "./movieList.json";
 
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <Header />
+        <div className="main">
+          {movies.map((element) => {
+            return (
+              <Movie
+                title={element.Title}
+                year={element.Year}
+                img={element.Poster}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
