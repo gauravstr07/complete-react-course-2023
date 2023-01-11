16e2c8e9120a3a36005d2c4dbe32d749bd016b46
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2023-01-11 18:50:09 +0530

    Deleted Setup for useState Project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
-  const [num, setNum] = useState(0);
-
-  function inc() {
-    setNum(num + 1);
-  }
-
-  function dec() {
-    setNum(num - 1);
-  }
-
-  return (
-    <div className="App">
-      <Header />
-
-      <div className="main">
-        <h1 className="heading">{num}</h1>
-        <div className="buttons"></div>
-        <button className="btn" onClick={inc}>
-          Increase
-        </button>
-        <button className="btn" onClick={dec}>
-          Decrease
-        </button>
-      </div>
-    </div>
-  );
+  return <div className="App">
+    <h1>Hello</h1>
+  </div>;
 }
 
 export default App;
