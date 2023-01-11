f2c01869cf572a5477535b1e61ead15f4f21eeb4
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2023-01-11 18:43:25 +0530

    useState Hook Explained | React.js Full Course #10

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Movie from "./components/Movie";
-import movies from "./movieList.json";
 
 function App() {
-  let login = true;
+  const [num, setNum] = useState(0);
+
+  function inc() {
+    setNum(num + 1);
+  }
+
+  function dec() {
+    setNum(num - 1);
+  }
 
   return (
     <div className="App">
-      {(() => {
-        if (!login) {
-          return (
-            <h1 style={{ color: "red" }}>
-              🔒 You don't have access to login this application 🔒
-            </h1>
-          );
-        }
-      })()}
       <Header />
+
       <div className="main">
-        {movies.map((element, index) => {
-          return (
-            <Movie
-              key={index}
-              title={element.Title}
-              year={element.Year}
-              img={element.Poster}
-            />
-          );
-        })}
+        <h1 className="heading">{num}</h1>
+        <div className="buttons"></div>
+        <button className="btn" onClick={inc}>
+          Increase
+        </button>
+        <button className="btn" onClick={dec}>
+          Decrease
+        </button>
       </div>
     </div>
   );
